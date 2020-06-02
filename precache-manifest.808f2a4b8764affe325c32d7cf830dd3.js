36fcb8f57040500404e0df7589bb4f979418c707
william <wwchen412@gmail.com>
2020-06-02 16:19:40 +0800

    Updates

---
+++ precache-manifest.808f2a4b8764affe325c32d7cf830dd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a54060688596f264562b94218f97b6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49c14f57bfb809fbe290",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3919f9317aeeadeefd96",
+    "url": "/static/css/main.af48241a.chunk.css"
+  },
+  {
+    "revision": "49c14f57bfb809fbe290",
+    "url": "/static/js/2.0583b886.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.0583b886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3919f9317aeeadeefd96",
+    "url": "/static/js/main.3e11cb2e.chunk.js"
+  },
+  {
+    "revision": "92df001c53e8b48f2c31",
+    "url": "/static/js/runtime-main.f81b0b24.js"
+  },
+  {
+    "revision": "e17cd51cad01c3c5e8cdcdd4c7c38ceb",
+    "url": "/static/media/index.e17cd51c.jpg"
+  }
+]);
